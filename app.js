50ba8213570af04437c3346d58670a99f0aa6d6b
Nasseima <lindsaynasseima@gmail.com>
2024-05-10 13:51:27 -0400

    Changes to check adult status to make it boolean

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+// Task #1
+// Implement checkAdultStatus function
 function checkAdultStatus(age){
-    if(age === 18){
-        console.log(`True`)
-    }
-    else{
-        console.log(`False`)
-    }
+// Parameter is age
+    let limit = 18 
+// Returns true if age is 18 or older.
+// Returns false if age is under 18.
+        console.log(limit === age)
 }
-checkAdultStatus(16)
+checkAdultStatus(18)
+// Initialize adultStatus variable.
+
