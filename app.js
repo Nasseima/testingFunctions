1774abb429b789e220f272e5d141c44ab9f59ab1
Nasseima <lindsaynasseima@gmail.com>
2024-05-10 14:55:36 -0400

    Implement calculate passing status and determine grade function

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
 // Task #1
-// Implement checkAdultStatus function
+// Implement checkAdultStatus function.
+// Parameter is age.
 function checkAdultStatus(age){
-// Parameter is age
-    let adultStatus = 18 
+// Initialize adultStatus variable.
+    const adultStatus = 18 
 // Returns true if age is 18 or older.
 // Returns false if age is under 18.
         console.log(age >= adultStatus)
 }
+// Expected results are Boolean.
 checkAdultStatus(25)
 checkAdultStatus(14)
-// Initialize adultStatus variable.
+
+
+// Test #2
+// Implement calculatePassingStatus function.
+// Parameter is score.
+function calculatePassingStatus(score){
+    const passingStatus = 70
+console.log(score >= passingStatus)
+}
+// Results are Boolean.
+calculatePassingStatus(60)
+calculatePassingStatus(80)
+
+// Test #3
+// Implement determinGrade function
+function determineGrade(score){
+    if(score <= 60){
+        console.log(`F`)
+    }
+    else if(score <= 70){
+        console.log(`D`)
+    }
+    else if(score <= 80){
+        console.log(`C`)
+    }
+    else if(score <= 90){
+        console.log(`B`)
+    }
+    else if(score <= 100){
+        console.log(`A`)
+    }
+    else if(score >= 100){
+        console.log(`A++`)
+    }
+}
+ determineGrade(65)
 
